591b46fd32885036f85d8f486ce739395ac6ea5a
nolliechyTW <nolliechy@gmail.com>
2024-02-24 15:58:12 -0500

    timer update

--- Scrumdinger/MeetingView.swift
+++ Scrumdinger/MeetingView.swift
@@ -1,44 +1,31 @@
-/*
-  计时器面板。
- */
-
 import SwiftUI
 import AVFoundation
 
 struct MeetingView: View {
     @Binding var scrum: DailyScrum
     @StateObject var scrumTimer = ScrumTimer()
-    @State private var transcript = ""
-    @State private var isRecording = false
-    private let speechRecognizer = SpeechRecognizer()
-    var player: AVPlayer { AVPlayer.sharedDingPlayer }
-    
     
     var body: some View {
         ZStack {
-            // background
             RoundedRectangle(cornerRadius: 16.0)
-                .fill(Color(red: 193/255, green: 242/255, blue: 176/255))
+                .fill(Color(red: 255/255, green: 243/255, blue: 207/255))
+//                .fill(Color(red: 89/255, green: 180/255, blue: 195/255))
             VStack(spacing: 20){
                 MeetingHeaderView(secondsElapsed: scrumTimer.secondsElapsed, secondsRemaining: scrumTimer.secondsRemaining, scrumColor: scrum.color)
-                
-                MeetingTimerView(scrumColor: scrum.color, countTo: scrumTimer.secondsRemaining)
-                
-                // MeetingFooterView(speakers: scrumTimer.speakers, skipAction: scrumTimer.skipSpeaker)
+                MeetingTimerView(scrumColor: scrum.color, scrumTimer: scrumTimer)
             }
-
         }
         .padding()
         .foregroundColor(scrum.color.accessibleFontColor)
         .onAppear {
             scrumTimer.reset(lengthInMinutes: scrum.lengthInMinutes)
+            scrumTimer.startScrum()
         }
         .onDisappear {
             scrumTimer.stopScrum()
             let newHistory = History(lengthInMinutes: scrumTimer.secondsElapsed / 60, lengthInHours: scrumTimer.secondsElapsed / 3600)
             scrum.history.insert(newHistory, at: 0)
         }
-        .navigationBarTitleDisplayMode(.inline)
     }
 }
 
