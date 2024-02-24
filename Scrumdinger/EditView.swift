a75edd46096256de2febd0546bac480bcebae405
nolliechyTW <nolliechy@gmail.com>
2024-02-24 17:25:30 -0500

    update edit view

--- Scrumdinger/EditView.swift
+++ Scrumdinger/EditView.swift
@@ -2,25 +2,6 @@ import SwiftUI
 
 struct EditView: View {
     @Binding var scrumData: DailyScrum.Data
-    @State private var selectedCategory: String = "work" // Temporary state, changed from selectedType
-    let categories = ["work", "health", "study", "custom"] // should come from backend, changed from types
-    @State private var customCategory: String = "" // State for custom category input, changed from customType
-    
-    // Define a method to get color based on category
-    private func colorForCategory(category: String) -> Color {
-        switch category {
-        case "work":
-            return Color.blue
-        case "health":
-            return Color.green
-        case "study":
-            return Color.yellow
-        case "custom":
-            return Color.purple
-        default:
-            return Color.gray
-        }
-    }
     
     var body: some View {
         List {
@@ -35,17 +16,9 @@ struct EditView: View {
                     Text("\(Int(scrumData.lengthInHours)) hours")
                         .accessibilityHidden(true)
                 }
-                Picker("Category", selection: $selectedCategory) { // Changed from "Type" to "Category"
-                    ForEach(categories, id: \.self) { category in
-                        Text(category.capitalized)
-                            .foregroundColor(colorForCategory(category: category))
-                    }
-                }
+                ColorPicker("Color", selection: $scrumData.color)
+                    .accessibilityLabel(Text("Color picker"))
 
-                if selectedCategory == "custom" {
-                    TextField("Custom Category", text: $customCategory)
-                        .foregroundColor(colorForCategory(category: "custom"))
-                }
             }
         }
         .listStyle(InsetGroupedListStyle())
